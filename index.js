aaf038c24602804011757569f51bd58ec3345a46
Mariam Adeyemo <adeyemomariamtobiloba@gmail.com>
2025-07-18 16:43:28 +0100

    index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(cors ({
     }
   },
   credentials:true,
-  methods:["GET","POST", "PUT", "DELETE"],
+  methods:["GET","POST", "PUT", "DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }))
 app.use((err, req, res, next) => {
